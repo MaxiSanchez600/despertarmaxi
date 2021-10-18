901a82810a78a936882c359ab3f828e333c7384c
MaxiSanchez600 <maxsanchezg@gmail.com>
2021-10-17 23:23:02 -0300

    make it better

---
+++ index.js
@@ -0,0 +1,38 @@
+const { exec } = require("child_process");
+const axios = require("axios");
+const port = process.env.PORT || 3001;
+var express = require("express");
+var app = express();
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+app.use(function (req, res, next) {
+  res.setHeader("Acces-Control-Allow-Origin", "*");
+  res.setHeader("Acces-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+  res.setHeader("Acces-Contorl-Allow-Methods", "Content-Type", "Authorization");
+  next();
+});
+app.post("/open", async function (req, res) {
+  console.log("ROCHO");
+  const { name, message } = req.body;
+  const response = await axios.post("http://181.12.248.55:8080/open", {
+    name: name,
+    message: message,
+  });
+  if (response.data) {
+    res.send("success");
+  } else {
+    res.status(400).send("error");
+  }
+});
+
+app.get("/health", async function (req, res) {
+  const response = await axios.get("http://181.12.248.55:8080/health");
+  if (response.data) {
+    res.send("ONLINE");
+  } else {
+    res.status(400).send("error");
+  }
+});
+
+app.listen(port);
